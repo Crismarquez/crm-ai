6cb154611f3e12c3769d8dd706f6e694212f4166
Cristian Marquez <cristian.dmr10@gmail.com>
2023-02-05 18:34:13 -0500

    add strong_sort project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # crm-ai
+
+### Virtual develop environment
+<prev>
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install --upgrade pip setuptools wheel
+python3 -m pip install -e ".[dev]"
+pre-commit install
+pre-commit autoupdate
+<prev>
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+# setup.py
+from pathlib import Path
+
+from setuptools import find_namespace_packages, setup
+
+# Load packages from requirements.txt
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt"), "r") as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
+
+docs_packages = ["mkdocs==1.3.0", "mkdocstrings==0.18.1"]
+
+style_packages = ["black==22.3.0", "flake8==3.9.2", "isort==5.10.1"]
+
+test_packages = ["pytest==7.1.2", "pytest-cov==2.10.1", "great-expectations==0.15.15"]
+
+
+# setup.py
+setup(
+    name="crm-vision",
+    version=0.1,
+    description="Face recognition project.",
+    author="BlueLabs",
+    author_email="bluelabs.ai@gmail.com",
+    url="https://bluelabsai.com/",
+    python_requires=">=3.8",
+    extras_require={
+        "dev": docs_packages + style_packages + test_packages + ["pre-commit==2.19.0"],
+        "docs": docs_packages,
+        "test": test_packages,
+    },
+)
